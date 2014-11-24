ba0fde67f9429b1b5de47b89d71c84b63997c228
Pedro Santana <psantana@mit.edu>
2014-11-24 16:52:40 -0500

    Improved docstrings on pygurls.pyx.

--- pygurls/extension/pygurls.pyx
+++ pygurls/extension/pygurls.pyx
@@ -37,19 +37,18 @@
 #  POSSIBILITY OF SUCH DAMAGE.
 #
 """
-A Python wrapper for GURLS++
+A Python wrapper for GURLS++.
 
-This is a Cython wrapper that allows GURLS++ functions to be called from 
-natively from within Python.
+Cython wrapper that allows GURLS++ to be called natively from within Python.
 
 @author: Pedro Santana (psantana@mit.edu).
 """ 
+
 import numpy as np
 cimport numpy as np
 import scipy.io
 from cython.view cimport array as cvarray
 from libcpp.vector cimport vector
-
 
 #Declares the C++ wrapper class
 cdef extern from "pygurls_wrapper.h" namespace "gurls":
@@ -75,35 +74,47 @@ cdef extern from "pygurls_wrapper.h" namespace "gurls":
         
         
 cdef class PyGURLS:
-    """Class that provides an Python interface to the functions in the C++
-    wrapper class."""
+    """Class that provides a Python interface to GURLS++."""
     cdef PyGURLSWrapper *thisptr
         
     def __cinit__(self,data_type=None,*args,**kwargs):
-        """Constructor for extension type."""        
-        #Initializes the GURLS++ wrapper object and reference appropriately        
+        """Constructor.
+
+        Optional arguments:
+        data_type -- either 'double' (default) or 'float'
+        """        
+                
         if data_type != None:
             self.thisptr = new PyGURLSWrapper(data_type) 
         else:
             self.thisptr = new PyGURLSWrapper() 
             
     def __dealloc__(self):
-        """Destructor for extension type."""
+        """Destructor."""
+        
         del self.thisptr
     
     def _gMat2D_to_np(self,gMat2D_vec):
+        """Convert from gMat2D type (GURLS++) to 2D NumPy array."""
+        
         vec_mat = np.array(gMat2D_vec)
         rows = self.thisptr.get_num_rows();
         cols = self.thisptr.get_num_cols();       
         return vec_mat.reshape((rows,cols),order='F')        
     
-    def get_field(self,field):            
+    def get_field(self,field):    
+        """Return field within a GurlsOptionsList structure."""        
+        
         return self._gMat2D_to_np(self.thisptr.get_field(field))
     
     def get_option_field(self,option,field):
+        """Return field within nested option in a GurlsOptionsList structure."""        
+        
         return self._gMat2D_to_np(self.thisptr.get_opt_field(option,field))
                          
     def add_data(self,np.ndarray[np.float64_t, ndim=2] mat2D, data_id):
+        """Add 2D NumPy array to GURLS++ pipeline with id=data_id."""
+        
         cdef vector[double] vec_mat = mat2D.flatten('F')       
         self.thisptr.add_data(vec_mat,
                               <unsigned long>mat2D.shape[0],
@@ -111,11 +122,17 @@ cdef class PyGURLS:
                               data_id)             
              
     def load_data(self,data_file,data_id):                
-        #Calls the C++ loading function for everything else                        
+        """Load file=data_file and add to GURLS++ pipeline with id=data_id."""
+                
         self.thisptr.load_data(data_file,data_id)         
 
     def import_mat_file(self,mat_file,var_names=[]):           
-        #MATLAB import
+        """Load MATLAB workspace file mat_file as Python dictionary.
+        
+        Optional arguments:        
+        var_names -- list of variable names to be imported (all by default).
+        """
+                
         if len(var_names)>0:
             data_dic = scipy.io.loadmat(mat_file,appendmat=True,
                                         variable_names=var_names)
@@ -124,34 +141,63 @@ cdef class PyGURLS:
         return data_dic 
     
     def get_data(self,data_id):
+        """Retrieve 2D matrix from GURLS++ with id=data_id as NumPy array."""        
+        
         return self._gMat2D_to_np(self.thisptr.get_data_vec(data_id))        
         
     def erase_data(self,data_id):
+        """Remove 2D matrix from GURLS++ with id=data_id."""
+        
         self.thisptr.erase_data(data_id)        
         
     def set_task_sequence(self,task_list):
+        """Set the sequence of tasks in the GURLS++ opt. pipeline.
+        
+        task_list = [ ['opt_1','arg_1'], ['opt_2','arg_2'], ... ]        
+        """
+        
         str_list = [p[0]+":"+p[1] for p in task_list]    
         self.thisptr.set_task_sequence("\n".join(str_list))
     
     def clear_task_sequence(self):
+        """Clear the sequence of tasks in the GURLS++ opt. pipeline."""
+        
         self.thisptr.clear_task_sequence()
     
     def add_process(self,p_name,opt_str_list):
+        """Add an optimization process to GURLS++."""
+        
         self.thisptr.add_process(p_name,"\n".join(opt_str_list))
     
-    def init_processes(self,p_name,use_default):        
+    def init_processes(self,p_name,use_default):     
+        """Initialize list of GURLS++ optimization processes."""
+        
         self.thisptr.init_processes(p_name,use_default)   
     
     def clear_processes(self):        
+        """Clear list of GURLS++ optimization processes."""
+        
         self.thisptr.clear_processes()
     
     def build_pipeline(self,p_name,use_default):
+        """Build the GURLS++ optimization pipeline."""
+        
         self.thisptr.build_pipeline(p_name,use_default)
     
     def clear_pipeline(self):
+        """Clear the GURLS++ optimization pipeline."""
+        
         self.thisptr.clear_pipeline()
     
     def run(self,in_data_id, out_data_id, job_id):
+        """Run a process with given input and output data.
+        
+        Mandatory arguments:
+        in_data_id -- string id of the input data.
+        out_data_id -- string id of the output data.
+        job_id -- string id of the process to be executed.   
+        """
+        
         return self.thisptr.run(in_data_id,out_data_id,job_id)    
         
     
